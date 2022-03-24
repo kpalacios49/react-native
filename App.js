5a3239b5c64aa7ec2764ae460bb4223fbccc00a5
Kevin <kevin49palacios@gmail.com>
2022-03-23 22:25:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Coder!</Text>
+      <Image 
+      style={{width: '100%', height: '50%'}}
+      source={{uri:'https://media3.giphy.com/media/xT5LMHxhOfscxPfIfm/giphy.gif?cid=ecf05e47gu8o845xpan5k8ktiwip5kfdf4fgpyfqq3wo3lwr&rid=giphy.gif&ct=g'}}
+      ></Image>
       <StatusBar style="auto" />
     </View>
   );
